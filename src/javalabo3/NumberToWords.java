d4b0e99d9a0214ccf38bd99b86fcb6e993fde14e
Chukwunonso <offorma@gmail.com>
2016-02-18 18:53:58 +0000

    init

--- src/javalabo3/NumberToWords.java
+++ src/javalabo3/NumberToWords.java
@@ -18,5 +18,272 @@ public class NumberToWords {
         System.out.println("please enter a number between 20 and 99");
         int number = input.nextInt();
         
-       }
+        int tens=number/10, units = number%10;
+       
+        String tensInWord = ""; 
+        
+        String unitsInWord = "";
+       
+        switch(tens){
+            case 2:
+                tensInWord = "Twenty";
+                switch(units){
+                    case 1:
+                          unitsInWord = "One";
+                           break;
+                    case 2:
+                        unitsInWord = "Two";
+                         break;
+                    case 3:
+                        unitsInWord = "Three";
+                         break;
+                    case 4:
+                        unitsInWord= "Four"; 
+                         break;
+                    case 5:
+                        unitsInWord = "Five";
+                         break;
+                    case 6:
+                        unitsInWord = "Six";
+                         break;
+                    case 7:
+                        unitsInWord = "Seven";
+                         break;
+                    case 8:
+                        unitsInWord = "Eight"; 
+                         break;
+                    case 9:
+                        unitsInWord = "Nine"; 
+                         break;
+                   }
+                break;
+            case 3:
+                tensInWord = "Thirty";
+                switch(units){
+                    case 1:
+                          unitsInWord = "One";
+                           break;
+                    case 2:
+                        unitsInWord = "Two";
+                         break;
+                    case 3:
+                        unitsInWord = "Three";
+                         break;
+                    case 4:
+                        unitsInWord= "Four"; 
+                         break;
+                    case 5:
+                        unitsInWord = "Five";
+                         break;
+                    case 6:
+                        unitsInWord = "Six";
+                         break;
+                    case 7:
+                        unitsInWord = "Seven";
+                         break;
+                    case 8:
+                        unitsInWord = "Eight"; 
+                         break;
+                    case 9:
+                        unitsInWord = "Nine"; 
+                         break;
+                        }
+                 break;
+            case 4:
+                tensInWord = "Forty"; 
+                switch(units){
+                    case 1:
+                          unitsInWord = "One";
+                           break;
+                    case 2:
+                        unitsInWord = "Two";
+                         break;
+                    case 3:
+                        unitsInWord = "Three";
+                         break;
+                    case 4:
+                        unitsInWord= "Four"; 
+                         break;
+                    case 5:
+                        unitsInWord = "Five";
+                         break;
+                    case 6:
+                        unitsInWord = "Six";
+                         break;
+                    case 7:
+                        unitsInWord = "Seven";
+                         break;
+                    case 8:
+                        unitsInWord = "Eight"; 
+                         break;
+                    case 9:
+                        unitsInWord = "Nine"; 
+                         break;
+                        }
+                 break;
+            case 5:
+                tensInWord = "Fifty";
+                switch(units){
+                    case 1:
+                          unitsInWord = "One";
+                           break;
+                    case 2:
+                        unitsInWord = "Two";
+                         break;
+                    case 3:
+                        unitsInWord = "Three";
+                         break;
+                    case 4:
+                        unitsInWord= "Four"; 
+                         break;
+                    case 5:
+                        unitsInWord = "Five";
+                         break;
+                    case 6:
+                        unitsInWord = "Six";
+                         break;
+                    case 7:
+                        unitsInWord = "Seven";
+                         break;
+                    case 8:
+                        unitsInWord = "Eight"; 
+                         break;
+                    case 9:
+                        unitsInWord = "Nine"; 
+                         break;
+                        }
+                 break;
+            case 6:
+                tensInWord = "Sixty";
+                    switch(units){
+                        case 1:
+                          unitsInWord = "One";
+                           break;
+                        case 2:
+                            unitsInWord = "Two";
+                             break;
+                        case 3:
+                            unitsInWord = "Three";
+                             break;
+                        case 4:
+                            unitsInWord= "Four"; 
+                             break;
+                        case 5:
+                            unitsInWord = "Five";
+                             break;
+                        case 6:
+                            unitsInWord = "Six";
+                             break;
+                        case 7:
+                            unitsInWord = "Seven";
+                             break;
+                        case 8:
+                            unitsInWord = "Eight"; 
+                             break;
+                        case 9:
+                            unitsInWord = "Nine"; 
+                             break;
+                            }
+                break;
+            case 7:
+                tensInWord = "Seventy";
+                switch(units){
+                    case 1:
+                          unitsInWord = "One";
+                           break;
+                    case 2:
+                        unitsInWord = "Two";
+                         break;
+                    case 3:
+                        unitsInWord = "Three";
+                         break;
+                    case 4:
+                        unitsInWord= "Four"; 
+                         break;
+                    case 5:
+                        unitsInWord = "Five";
+                         break;
+                    case 6:
+                        unitsInWord = "Six";
+                         break;
+                    case 7:
+                        unitsInWord = "Seven";
+                         break;
+                    case 8:
+                        unitsInWord = "Eight"; 
+                         break;
+                    case 9:
+                        unitsInWord = "Nine"; 
+                         break;
+                        }
+                 break;
+            case 8:
+                tensInWord = "Eighty";
+                switch(units){
+                    case 1:
+                        unitsInWord = "One";
+                        break;
+                    case 2:
+                        unitsInWord = "Two";
+                         break;
+                    case 3:
+                        unitsInWord = "Three";
+                         break;
+                    case 4:
+                        unitsInWord= "Four"; 
+                         break;
+                    case 5:
+                        unitsInWord = "Five";
+                         break;
+                    case 6:
+                        unitsInWord = "Six";
+                         break;
+                    case 7:
+                        unitsInWord = "Seven";
+                         break;
+                    case 8:
+                        unitsInWord = "Eight"; 
+                         break;
+                    case 9:
+                        unitsInWord = "Nine"; 
+                         break;
+                }
+                 break;
+            case 9:
+                tensInWord = "Ninety"; 
+                    switch(units){
+                      case 1:
+                          unitsInWord = "One";
+                           break;
+                      case 2:
+                          unitsInWord = "Two";
+                           break;
+                      case 3:
+                          unitsInWord = "Three";
+                           break;
+                      case 4:
+                          unitsInWord= "Four"; 
+                           break;
+                      case 5:
+                          unitsInWord = "Five";
+                           break;
+                      case 6:
+                          unitsInWord = "Six";
+                           break;
+                      case 7:
+                          unitsInWord = "Seven";
+                           break;
+                      case 8:
+                          unitsInWord = "Eight"; 
+                           break;
+                      case 9:
+                          unitsInWord = "Nine"; 
+                           break;
+                          }
+                 break;
+            }  
+        
+        System.out.println(tensInWord + unitsInWord);
+        
+    }
 }
